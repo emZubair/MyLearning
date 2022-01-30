6712a0247cfdd908b29e45983190acd73f9513cd
emzubair <muhammad.zubair@arbisoft.com>
2022-01-31 00:17:36 +0500

    Async chat added

--- edx/chat/consumers.py
+++ edx/chat/consumers.py
@@ -1,38 +1,45 @@
 from json import loads, dumps
+from django.utils import timezone
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
-        async_to_sync(self.channel_layer.group_add)(
+        # join the group
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
         # Accept connection
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
+    async def disconnect(self, code):
         #  leave the group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         """ Receive message from Web-socket """
 
+        now = timezone.now()
         text_data_json = loads(text_data)
         message = text_data_json.get('message')
         # send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
+                'user': self.user.username,
+                'datetime': now.isoformat(),
                 'type': 'chat_message',
                 'message': message
             }
         )
 
-    def chat_receive(self, event):
+    async def chat_message(self, event):
         # Send message to web socket
-        self.send(text_data=dumps(event))
+        await self.send(text_data=dumps(event))
