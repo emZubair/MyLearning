eec876eaec9cf77cc7f8d168ed5da12323b3797f
Zubair <36886070+emzubair@users.noreply.github.com>
2022-02-08 10:30:04 +0500

    Management commands, multiple settings, envrc to point to settings (#14)

--- edx/common/utils.py
+++ edx/common/utils.py
@@ -7,3 +7,15 @@ def get_item_from_cache(item_key):
 
 def set_item_in_cache(item_key, item, time_out=None):
     cache.set(item_key, item)
+
+
+def my_middleware(get_response):
+    def middleware(request):
+        # Code executed for each request before
+        # the view (and later middleware) are called.
+
+        response = get_response(request)
+        # Code executed for each request/response after
+        # the view is called.
+        return response
+    return middleware
